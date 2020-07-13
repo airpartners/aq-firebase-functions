0ee79966ea631432f339885dc040fe2608699000
Nina Tchirkova <nina.tchirkova@students.olin.edu>
2020-07-13 14:32:31 -0400

    scheduled functions

--- functions/index.js
+++ functions/index.js
@@ -74,10 +74,10 @@ async function getToken() {
 }
 
 async function writeDataToDB(data) {
-  console.log("restructuring data and writing to DB");
   const resultLength = data['meta']['per_page'];
   const sn = data['data'][0]['sn'];
   var snRef = admin.database().ref(sn);
+  console.log(`restructuring data for ${sn} and writing to DB`);
   snRef.child('latest').set(data['data'][0]);
   for (i = 0; i < resultLength; i++) {
     snRef.child('/data/'+data['data'][i]['timestamp']).set(data['data'][i]);
@@ -88,8 +88,8 @@ function getEndpoint(deviceId = 'SN000-072', page = 1, perPage = 2) {
   return `${BASE_URL}/${deviceId}/data/?page=${page}&per_page=${perPage}&limit=${LIMIT}`;
 }
 
-exports.fetchQuantAQ = functions.https.onRequest((request, response) => {
-  getToken().then(result => fetch(getEndpoint('SN000-088', 1, 10), {
+function getDataAndWriteToDB(sn) {
+  getToken().then(result => fetch(getEndpoint(sn, 1, 10), {
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
@@ -100,13 +100,40 @@ exports.fetchQuantAQ = functions.https.onRequest((request, response) => {
   })).then(res => res.json())
     .then(data => writeDataToDB(data))
     .catch(error => console.log(error));
-  response.send("Fetch is running asynchronously! The data will be printed to the console when it's done.");
+}
+
+// This needs to be updated when EB devices go live
+const device_list = ['SN000-088', 'SN000-089', 'SN000-082', 'SN000-085', 'SN000-094', 'SN000-081'];
+
+exports.fetchQuantAQScheduled = functions.pubsub.schedule('every 10 minutes').onRun((context) => {
+  console.log("Fetching data from QuantAQ and writing to DB");
+  for (sn of device_list) {
+    getDataAndWriteToDB(sn);
+  }
+  return null;
+})
+
+exports.fetchQuantAQ = functions.https.onRequest((request, response) => {
+  for (sn of device_list) {
+    getDataAndWriteToDB(sn);
+  }
+  response.send("Fetch is running asynchronously! The data will be in the database when it's done.");
 } )
 
 exports.clearQuantAQ = functions.https.onRequest((request, response) => {
-  console.log("Clearing data from SN000-088");
-  admin.database().ref('SN000-088').set(null);
+  for (sn of device_list) {
+    console.log(`Clearing data from ${sn}`);
+    admin.database().ref(sn).set(null);
+  }
   response.send("Running asynchronously! Data will be cleared when it is done.")
+})
+
+exports.clearQuantAQScheduled = functions.pubsub.schedule('every 24 hours').onRun((context) => {
+  for (sn of device_list) {
+    console.log(`Clearing data from ${sn}`);
+    admin.database().ref(sn).set(null);
+  }
+  return null;
 })
 
 exports.accessSecret = functions.https.onRequest((request, response) => {
